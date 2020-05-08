acb3301b68dd7d8f6c8a45ef8eec49212039bad1
Florence Braddy <florencebraddy@hotmail.com>
2020-05-07 23:02:17 -0400

    mui NavBar component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,70 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-//import { Card } from "@material-ui/core";
+import { Card } from "@material-ui/core";
+import Nav from "./components/Nav";
 
 function App() {
-  const [show, setShow] = useState([]);
+  const [shows, setShows] = useState([]);
   const [toggle, setToggle] = useState(true);
   const [query, setQuery] = useState("Vampire");
   console.log(query);
+
   useEffect(() => {
     async function getData() {
-      const response = await axios.get(
-        `http://api.tvmaze.com/search/shows?q=${query}`
-      );
-      console.log(response.data[0].show);
-      setShow(response.data.show);
+      try {
+        const response = await axios.get(
+          `http://api.tvmaze.com/search/shows?q=${query}`
+        );
+        // console.log(response.data.show);
+        setShows(response.data.shows);
+      } catch (err) {
+        console.log(err);
+      }
     }
     getData();
-  }, [toggle]);
+  }, [toggle, query]);
 
   return (
     <div className="App">
+      <Nav setQuery={setQuery} setToggle={setToggle} toggle={toggle} />
+      <div>
+        {shows === undefined ? (
+          <div>LOADING...</div>
+        ) : (
+          shows.map(show => {
+            return (
+              // <div key={show.id} show={show}>
+              <Card key={show.id}>
+                <p>Hello</p>
+
+                {/* <h3>{show.url}</h3>
+                  <h3>{show.language}</h3> */}
+              </Card>
+            );
+          })
+        )}
+      </div>
       <header className="App-header">
-        <input onChange={event => setQuery(event.target.value)} />
-        <button onClick={() => setToggle(!toggle)}>Search</button>
-        {/* <p>{query}</p> */}
-        <div className="main-content">
-          {show &&
-            show.map(show => {
+        {/* <input onChange={event => setQuery(event.target.value)} />
+        <button onClick={() => setToggle(!toggle)}>Search</button> */}
+        <p>{query}</p>
+        <div>
+          {shows === undefined ? (
+            <div>LOADING...</div>
+          ) : (
+            shows.map(show => {
               return (
-                <div key={show.id} show={show}>
-                  <h1>{show.name}</h1>
+                // <div key={show.id} show={show}>
+                <Card key={show.id}>
+                  <p>Hello</p>
 
                   {/* <h3>{show.url}</h3>
                   <h3>{show.language}</h3> */}
-                </div>
+                </Card>
               );
-            })}
+            })
+          )}
         </div>
       </header>
     </div>
---
+++ src/components/Nav.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import InputBase from "@material-ui/core/InputBase";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+export default function Nav({ setQuery, toggle, setToggle }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Movies
+          </Typography>
+          <InputBase
+            onChange={event => setQuery(event.target.value)}
+            placeholder="Search for a movie..."
+            classes={{ root: classes.inputRoot, input: classes.inputInput }}
+          />
+          <Button style={{ color: "#fff" }} onClick={() => setToggle(!toggle)}>
+            Search
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
